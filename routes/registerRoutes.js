685ab72dda10743e74813404c04a621a71a661f2
Vrutik <vrutikprajapati2809@gmail.com>
2021-07-28 01:29:33 +0530

    signup bug fixed

--- routes/registerRoutes.js
+++ routes/registerRoutes.js
@@ -8,11 +8,18 @@ router.get('/register',(req,res) =>{
 
 router.post('/register',(req,res) =>{
     const user = new User(req.body);
-    user.save()
-        .then(() => {
-            res.redirect('/login');
+    User.findOne(req.body)
+        .then((result) => {
+            if(result == null){
+                res.status(404).render('404',{title : "Error | 404"})
+            }
+            user.save()
+                .then(() => {
+                    res.redirect('/login');
+                })
+                .catch(err => console.log(err));
         })
-        .catch(err => console.log(err));
+        .catch((err) => console.log(err));
 })
 
 module.exports = router;
